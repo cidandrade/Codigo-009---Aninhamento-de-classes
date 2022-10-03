28f8efc59aeac2e76888b92a45ac64a2eb2bd6a5
Cid R. Andrade <profandrade@gmail.com>
2022-10-03 00:52:37 -0300

    Versão 1.0.22.10.03 Classes aninhadas

---
+++ src/br/com/cidandrade/aulas/AninhamentoApp.java
@@ -0,0 +1,11 @@
+package br.com.cidandrade.aulas;
+
+public class AninhamentoApp {
+
+    // Classes aninhadas
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(carro);
+    }
+
+}
